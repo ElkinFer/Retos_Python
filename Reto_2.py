15fcb4b0a12c9b529dfc65e9bab69d0021812941
ElkinFer <125397673+ElkinFer@users.noreply.github.com>
2023-03-11 07:51:07 -0500

    Update Reto_2.py

--- Reto_2.py
+++ Reto_2.py
@@ -2,6 +2,6 @@ print ("Hola enfermera")
 
 peso = int (input("Ingrese el peso del bebe: "))
 edad = int(input("Ingrese la edad del bebé en meses: "))
-dosis = ((peso+10)/(edad*10))*8
+dosis = ((peso+10)/(meses*10))*8
 
 print ("La dosis a aplicar es = ",dosis)
